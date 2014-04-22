b5763a30d53baeb5374cab84cba7460c6b38ae8e
j335632 <j335632@hotmail.com>
2014-04-22 11:35:44 +0900

    aaa

--- namoo-club4-web/src/main/java/com/namoo/club/web/controller/CommunityController.java
+++ namoo-club4-web/src/main/java/com/namoo/club/web/controller/CommunityController.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.namoo.club.domain.entity.Community;
@@ -43,4 +44,21 @@ public class CommunityController {
 		mav.setViewName("community/home");
 		return mav;
 	}
+	
+	@RequestMapping(value="cmcreate", method= RequestMethod.GET)
+	public ModelAndView communityCreate(HttpServletRequest req) {
+		//
+		
+		ModelAndView mav = new ModelAndView();
+		
+		String cmId = req.getParameter("cmId");	
+		mav.addObject("cmId", cmId);
+		
+		
+		mav.setViewName("community/create");
+		
+		return mav;
+		
+	}
+	
 }
